faf80e0a892906b72479895f12cec7a9c35de907
Todor <todordelyanovslavov72>
2018-09-05 17:28:33 +0300

    ListAll Activity, Contracts, Presenter

--- app/src/main/java/com/example/mihai/moviequotesapp/views/activities/ListAllQuotesActivity.java
+++ app/src/main/java/com/example/mihai/moviequotesapp/views/activities/ListAllQuotesActivity.java
@@ -2,16 +2,29 @@ package com.example.mihai.moviequotesapp.views.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.example.mihai.moviequotesapp.QuotesAdapter;
 import com.example.mihai.moviequotesapp.R;
+import com.example.mihai.moviequotesapp.async.base.AsyncRunner;
+import com.example.mihai.moviequotesapp.models.Quote;
+import com.example.mihai.moviequotesapp.repositories.HttpRepository;
+import com.example.mihai.moviequotesapp.services.base.QuoteService;
+import com.example.mihai.moviequotesapp.views.contracts.ListAllQuotesContracts;
+import com.example.mihai.moviequotesapp.views.presenters.ListAllQuotesPresenter;
+
+import java.util.List;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class ListAllQuotesActivity extends DaggerAppCompatActivity {
+public class ListAllQuotesActivity extends DaggerAppCompatActivity implements ListAllQuotesPresenter.Presenter{
+    private QuoteService mQuoteService;
+    private AsyncRunner mAsyncRunner;
+    private ListAllQuotesContracts.View mView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_all_quotes);
+    public void subscribe(ListAllQuotesContracts.View view) {
+        mView=view;
     }
+
 }
