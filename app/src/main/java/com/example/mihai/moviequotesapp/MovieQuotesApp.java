0787178171ccb2aedceb472e1aec797801f8d4bf
Todor <todordelyanovslavov72>
2018-09-05 16:28:00 +0300

    ListAll Activity, Contracts, Presenter

--- app/src/main/java/com/example/mihai/moviequotesapp/MovieQuotesApp.java
+++ app/src/main/java/com/example/mihai/moviequotesapp/MovieQuotesApp.java
@@ -8,6 +8,7 @@ import dagger.android.DaggerApplication;
 public class MovieQuotesApp extends DaggerApplication{
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return DaggerAppComponent.builder().application(this).build();
+    return DaggerAppComponent.builder().application(this).build();
+
     }
 }
